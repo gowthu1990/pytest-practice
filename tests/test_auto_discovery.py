cc7cf9122fa341fda87c60d6721a5415a530cb01
Sai Gowtham Badvity <gowtham.badvity@gmail.com>
2025-09-02 14:03:40 +0530

    Updates

--- tests/test_auto_discovery.py
+++ tests/test_auto_discovery.py
@@ -7,15 +7,24 @@
 """
 
 def test_addition():
+    """
+    unit test for addition
+    """
     print("\ntest addition")
     assert 1 + 1 == 2
 
 def test_subtraction():
+    """
+    unit test for subtraction
+    """
     print("\ntest subtraction")
     result = 5 - 3
     assert result == 2, "Subtraction test failed!" # The string is an optional error message
 
 def some_other_function():
+    """
+    This is ignored
+    """
     print("\nIgnored tests")
     # This function will be ignored by pytest
     pass
